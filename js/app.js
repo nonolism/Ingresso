aa8d8194e66f3fb534f639d621bfca176d0323e0
Nonolism <70848785+nonolism@users.noreply.github.com>
2024-09-09 00:47:57 -0300

    Adicionar função comprar e forma de diminuir a quantidade disponivel de ingressos

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//permitir que o usuário compre uma quantidade n de ingressos do tipo selecionado
+//subtrair do total, a quantidade que foi comprada
+//caso seja selecionado uma quantidade maior que o total, não comprar e enviar um alerta ao usuário
+function comprar(){
+    let tipoIngresso = document.getElementById('tipo-ingresso').value
+    let quantidade = document.getElementById('qtd').value;
+    let itemDeComparacao = document.getElementById(`qtd-${tipoIngresso}`);
+    let quantidadeItemDeComparacao = parseInt(itemDeComparacao.innerText)
+
+    if (quantidadeItemDeComparacao > quantidade){
+        itemDeComparacao.innerText = quantidadeItemDeComparacao - quantidade;
+    } else {
+        alert('Quantidade inválida, selecione uma quantidade menor que o total');
+    }
+}
