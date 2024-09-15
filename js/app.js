3b4dc15d70111439222d7774ff603321f156ccb4
Nonolism <70848785+nonolism@users.noreply.github.com>
2024-09-15 00:49:42 -0300

    Remover comentários

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-//permitir que o usuário compre uma quantidade n de ingressos do tipo selecionado
-//subtrair do total, a quantidade que foi comprada
-//caso seja selecionado uma quantidade maior que o total, não comprar e enviar um alerta ao usuário
 function comprar(){
     let tipoIngresso = document.getElementById('tipo-ingresso').value
     let quantidade = document.getElementById('qtd').value;
