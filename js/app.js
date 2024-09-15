af82abf3f2706b3c38052d847c77d2eb28c3e8dd
Nonolism <70848785+nonolism@users.noreply.github.com>
2024-09-15 02:39:09 -0300

    Adicionar validação para não inserção de valores negativos

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 function comprar(){
-    let tipoIngresso = document.getElementById('tipo-ingresso').value
-    let quantidade = document.getElementById('qtd').value;
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = document.getElementById('qtd');
     let itemDeComparacao = document.getElementById(`qtd-${tipoIngresso}`);
-    let quantidadeItemDeComparacao = parseInt(itemDeComparacao.innerText)
+    let quantidadeItemDeComparacao = parseInt(itemDeComparacao.innerText);
 
-    if (quantidadeItemDeComparacao > quantidade){
-        itemDeComparacao.innerText = quantidadeItemDeComparacao - quantidade;
+    if (quantidadeItemDeComparacao > 0){
+        if (quantidadeItemDeComparacao >= quantidade.value){
+            if (quantidade.value >= 0){
+            itemDeComparacao.innerText = quantidadeItemDeComparacao - quantidade.value;
+            } else {
+                alert('Quantidade solicitada invalida, insira um valor positivo');
+            }
+        } else {
+            alert(`Quantidade inválida, selecione uma quantidade menor ou igual a ${quantidadeItemDeComparacao}`);
+            quantidade.value = '';
+        }
     } else {
-        alert(`Quantidade inválida, selecione uma quantidade menor ou igual a ${quantidadeItemDeComparacao}`);
+        alert('Quantidade de ingressos para essa posição esgotados');
     }
 }
